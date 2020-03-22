0c0ff2cac8dcfbff817aff3d19a3cd9afe8f8d21
Huiyu <iownthegame@gmail.com>
2020-03-22 16:19:31 +0100

    add nodejs backend and js frontend code

--- app.js
+++ app.js
@@ -2,16 +2,37 @@ const express = require('express')
 const app = express()
 const port = 8000
 
+if(process.env.NODE_ENV === 'development') {
+  const host = 'http://localhost:8080'
+  // Add headers
+  app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', host);
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+  });
+}
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 
-const {JSZhuyin} = require('jszhuyin');
-var jszhuyin = new JSZhuyin();
-jszhuyin.load()
+const { JSZhuyin } = require('jszhuyin');
 
-let mapping = {
+
+const mapping = {
 	'q':	'ㄆ',
 	'w':	'ㄊ',
 	'e':	'ㄍ',
@@ -55,30 +76,47 @@ let mapping = {
 	'-':	'ㄦ',
 }
 
-var candidates = [];
-jszhuyin.oncandidateschange = function(c) {
-  candidates = c;
-};
-
 // app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/', (req, res) => res.sendFile('index.html', { root: __dirname }))
-app.post('/', (req, res) => {
-  let key = req.body.key
+app.get('/api', (req, res) => {
+  let key = req.query.key
+  // console.log(`key=${key}`)
 
-
-  let string = ""
-  for (let i = 0; i < key.length; i++) {
-    if (key[i] in mapping) {
-      string += mapping[key[i]]
+  let string = key.split('').map(k => {
+    if (k in mapping) {
+      return mapping[k]
     }
+    if (Object.values(mapping).indexOf(k) > -1) {
+      return k
+    }
+    return ''
+  }).join('')
+  // console.log(`string=${string}`)
+
+  if (!string) {
+    return res.json({
+      status: 0,
+      zhuyin: '',
+      chinese: ''
+    })
   }
+
+  let jszhuyin = new JSZhuyin()
+  let candidates = []
+
+  jszhuyin.load()
+  jszhuyin.oncandidateschange = function(c) {
+    candidates = c
+  };
 
   jszhuyin.handleKey(string)
   let answer = candidates[0][0]
-  candidates.pop()
-  // clear candidates
 
-  res.send(`key=${key}, string=${string}, answers=${answer}`)
+  res.json({
+    status: 0,
+    zhuyin: string,
+    chinese: answer
+  })
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
